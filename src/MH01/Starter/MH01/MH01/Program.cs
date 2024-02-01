bccd9c4cbe497e00949d8b58a2e8af032ef8ba1e
Vulcan <vulcan.lee@gmail.com>
2024-02-01 16:29:00 +0800

    動手建立多執行緒挑戰 – 完成原始碼

---
+++ src/MH01/Starter/MH01/MH01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace MH01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            #region 輸出 100 次的 World!
+            for (int i = 0; i < 100; i++)
+            {
+                System.Console.Write("Hello ");
+                Thread.Sleep(50);
+            }
+            #endregion
+
+            #region 輸出 100 次的 Hello
+            for (int i = 0; i < 100; i++)
+            {
+                System.Console.Write("World! ");
+                Thread.Sleep(50);
+            }
+            #endregion
+
+            sw.Stop();
+            System.Console.WriteLine("\n\nElapsed={0}", sw.Elapsed);
+        }
+    }
+}
