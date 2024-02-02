d8e71195ad76826878dce429a2b29c238d44745f
Vulcan <vulcan.lee@gmail.com>
2024-02-02 11:38:21 +0800

    .

---
+++ src/MH06/Finish - AutoResetEvent/MH06/MH06/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace MH06;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int counter = 0;
+        int max = int.MaxValue/4;
+        AutoResetEvent autoResetEvent = new AutoResetEvent(true);
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        Thread thread1 = new Thread(() =>
+        {
+            for (int i = 0; i < max; i++)
+            {
+                autoResetEvent.WaitOne();
+                counter++;
+                autoResetEvent.Set();
+            }
+        });
+        Thread thread2 = new Thread(() =>
+        {
+            for (int i = 0; i < max; i++)
+            {
+                autoResetEvent.WaitOne();
+                counter--;
+                autoResetEvent.Set();
+            }
+        });
+        thread1.Start(); thread2.Start();
+        thread1.Join(); thread2.Join();
+        sw.Stop();
+        Console.WriteLine($"Time: {sw.Elapsed}  Counter: {counter}");
+    }
+}
