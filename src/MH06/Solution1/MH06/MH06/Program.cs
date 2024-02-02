589c1d1b4b05c6e1f65cbbd082363a121ef4ec74
Vulcan <vulcan.lee@gmail.com>
2024-02-02 09:57:43 +0800

    |MH06|將多執行緒程式碼設計成為具有執行緒安全的特性|修正因為競賽造成共享變數錯亂並形成不具有執行緒安全特性|

---
+++ src/MH06/Solution1/MH06/MH06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace MH06;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int counter = 0;
+        int max = int.MaxValue;
+        object locker = new object();
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        Thread thread1 = new Thread(() =>
+        {
+            for (int i = 0; i < max; i++)
+            {
+                lock (locker)
+                {
+                    counter++;
+                }
+            }
+        });
+        Thread thread2 = new Thread(() =>
+        {
+            for (int i = 0; i < max; i++)
+            {
+                lock (locker)
+                {
+                    counter--;
+                }
+            }
+        });
+        thread1.Start(); thread2.Start();
+        thread1.Join(); thread2.Join();
+        sw.Stop();
+        Console.WriteLine("Counter: " + counter);
+        Console.WriteLine("Time: " + sw.Elapsed);
+    }
+}
