ed99f128eaf32aedc2add9fad6092b1204c580e2
Vulcan <vulcan.lee@gmail.com>
2024-02-03 23:37:32 +0800

    如何修正不會有打死結問題發生

---
+++ src/MH05/Finish/MH05/MyService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MH05
+{
+    public class MyService
+    {
+        public async Task StartAsync()
+        {
+            int beforeAsyncThreadId = Environment.CurrentManagedThreadId;
+            var result = GetAsync().Result;
+            int afterAsyncThreadId = Environment.CurrentManagedThreadId;
+            Debug.WriteLine($"[StartAsync] Before async: {beforeAsyncThreadId}, after async: {afterAsyncThreadId}");
+            await Task.Delay(result / 20);
+        }
+
+        public async Task<int> GetAsync()
+        {
+            int beforeAsyncThreadId = Environment.CurrentManagedThreadId;
+            var result = await new HttpClient()
+                .GetStringAsync("https://www.google.com").ConfigureAwait(false);
+            int afterAsyncThreadId = Environment.CurrentManagedThreadId;
+            Debug.WriteLine($"[GetAsync] Before async: {beforeAsyncThreadId}, after async: {afterAsyncThreadId}");
+            return result.Length;
+        }
+    }
+}
---
+++ src/MH05/Starter/MH05/MyService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MH05
+{
+    public class MyService
+    {
+        public async Task StartAsync()
+        {
+            var result = GetAsync().Result;
+            await Task.Delay(result / 20);
+        }
+
+        public async Task<int> GetAsync()
+        {
+            var result = await new HttpClient()
+                .GetStringAsync("https://www.google.com");
+            return result.Length;
+        }
+    }
+}
