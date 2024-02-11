6d644efb3da5ccc7d5e835067f8ae0006bb7fa55
Vulcan <vulcan.lee@gmail.com>
2024-02-12 01:26:10 +0800

    MH09|同時進行多個非同步方法

---
+++ src/MH09/Starter/MH09/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace MH09
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var gaussianBlur = new GaussianBlur();
+            await gaussianBlur.ProcessAsync();
+            stopwatch.Stop();
+            Console.WriteLine($"花費時間: {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
